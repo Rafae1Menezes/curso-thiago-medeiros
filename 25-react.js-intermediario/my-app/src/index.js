0a11a5752201d4047ffab12a53f9db885d709529
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-15 16:59:29 -0300

    adicionando template

--- 25-react.js-intermediario/my-app/src/index.js
+++ 25-react.js-intermediario/my-app/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createTheme} from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core'
+import { lightBlue, orange } from '@material-ui/core/colors';
+
 import App from './App';
+import './index.css'
+
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: lightBlue[900]
+    },
+    secondary: {
+      main: orange[900]
+    }
+  }
+})
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
