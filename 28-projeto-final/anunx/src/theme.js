2f1079c78c44f1a84c595add0c1c1507b5f7fc46
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-20 15:58:41 -0300

    adicionando pagina de publicar anuncio

--- 28-projeto-final/anunx/src/theme.js
+++ 28-projeto-final/anunx/src/theme.js
@@ -1,9 +1,14 @@
+import { red } from "@material-ui/core/colors";
 import { createTheme } from "@material-ui/core/styles";
 
 const theme = createTheme({
    palette: {
       primary: {
          main: '#000000',
+      },
+      background: {
+         default: 'rgb(242, 244, 245)',
+         white: '#FFFFFF'
       }
    }
 })
