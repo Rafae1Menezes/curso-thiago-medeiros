11a494ff9b72e4fa00f0fc4178d3645bea7a367d
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-20 15:05:42 -0300

    cria cards

--- 28-projeto-final/anunx/pages/user/dashboard.js
+++ 28-projeto-final/anunx/pages/user/dashboard.js
@@ -1,10 +1,21 @@
-import { Button, Container, Typography } from '@material-ui/core'
+import {
+   Button,
+   Card,
+   CardActions,
+   CardContent,
+   CardMedia,
+   Container,
+   Grid,
+   Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import TemplateDefault from '../../src/templates/Default'
 
 const useStyles = makeStyles(theme => ({
    container: {
       padding: theme.spacing(8, 0, 6),
+   },
+   cardMedia: {
+      paddingTop: '56%'
    },
    buttonAdd: {
       margin: '30px auto',
@@ -24,6 +35,85 @@ export default function Dashboard() {
             <Button variant="contained" color="primary" className={classes.buttonAdd}>
                Públicar novo anúncio
             </Button>
+         </Container>
+         <Container maxWidth="md">
+            <Grid container spacing={4}>
+               <Grid item xs={12} sm={6} md={4}>
+                  <Card>
+                     <CardMedia
+                        image={'https://source.unsplash.com/random'}
+                        title="Título da imagem"
+                        className={classes.cardMedia}
+                     />
+                     <CardContent>
+                        <Typography variant="h5" component="h2">
+                           Produto X
+                        </Typography>
+                        <Typography>
+                           R$ 60,00
+                        </Typography>
+                     </CardContent>
+                     <CardActions>
+                        <Button size="small" color="primary">
+                           Editar
+                        </Button>
+                        <Button size="small" color="primary">
+                           Remover
+                        </Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
+               <Grid item xs={12} sm={6} md={4}>
+                  <Card>
+                     <CardMedia
+                        image={'https://source.unsplash.com/random'}
+                        title="Título da imagem"
+                        className={classes.cardMedia}
+                     />
+                     <CardContent>
+                        <Typography variant="h5" component="h2">
+                           Produto X
+                        </Typography>
+                        <Typography>
+                           R$ 60,00
+                        </Typography>
+                     </CardContent>
+                     <CardActions>
+                        <Button size="small" color="primary">
+                           Editar
+                        </Button>
+                        <Button size="small" color="primary">
+                           Remover
+                        </Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
+               <Grid item xs={12} sm={6} md={4}>
+                  <Card>
+                     <CardMedia
+                        image={'https://source.unsplash.com/random'}
+                        title="Título da imagem"
+                        className={classes.cardMedia}
+                     />
+                     <CardContent>
+                        <Typography variant="h5" component="h2">
+                           Produto X
+                        </Typography>
+                        <Typography>
+                           R$ 60,00
+                        </Typography>
+                     </CardContent>
+                     <CardActions>
+                        <Button size="small" color="primary">
+                           Editar
+                        </Button>
+                        <Button size="small" color="primary">
+                           Remover
+                        </Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
+            </Grid>
          </Container>
       </TemplateDefault>
    )
