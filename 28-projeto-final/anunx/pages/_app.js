f75cee12e0c30a4f08062d0cf59c06866776ac22
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-20 10:36:22 -0300

    estrutura inicial + material ui

--- 28-projeto-final/anunx/pages/_app.js
+++ 28-projeto-final/anunx/pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import theme from '../src/theme'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+   const { Component, pageProps } = props
+
+   /* useEffect(() => {
+      //Remove the server-side injected css;
+      const jssStyles = document.querySelector('#jss-server-side')
+      if (jssStyles) {
+         jssStyles.parentElement.removeChild(jssStyles)
+      }
+   }, []) */
+
+   return (
+      <>
+         <Head>
+            <title>My Page</title>
+            <meta
+               name="viewport"
+               content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+         </Head>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+         </ThemeProvider>
+      </>
+   )
 }
 
-export default MyApp
+MyApp.propTypes = {
+   Component: PropTypes.elementType.isRequired,
+   pageProps: PropTypes.object.isRequired,
+}
