e7328224ef4d5d5aa552d1487be08bea76b629d7
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-21 09:37:43 -0300

    adiciona menu

--- 28-projeto-final/anunx/src/components/Header.js
+++ 28-projeto-final/anunx/src/components/Header.js
@@ -1,40 +1,94 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Link from 'next/link'
+import {
+   makeStyles,
+   AppBar,
+   Toolbar,
+   Typography,
+   Button,
+   IconButton,
+   Container,
+   Avatar,
+   Menu,
+   MenuItem,
+   Divider,
+} from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import MenuIcon from '@material-ui/icons/Menu'
+import { AccountCircle } from '@material-ui/icons'
+import { useState } from 'react'
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
+const useStyles = makeStyles(theme => ({
+   menuButton: {
+      marginRight: theme.spacing(2),
+   },
+   title: {
+      flexGrow: 1,
+   },
+   userName: {
+      marginLeft: 6
+   },
+   devider: {
+      margin: '8px 0'
+   },
+}))
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+export default function ButtonAppBar() {   
+   const classes = useStyles()
+   const [anchorUserMenu, setAnchorUserMenu] = useState(false)
+
+   const openUserMenu = Boolean(anchorUserMenu) 
+
+   return (
+      <>
+         <AppBar position="static" elevation={3}>
+            <Container maxWidth="lg">
+               <Toolbar>
+                  <Link href="/" passHref>                  
+                     <Typography variant="h6" className={classes.title}>
+                        AnuX
+                     </Typography>
+                  </Link>
+                  <Link href="/user/publish" passHref>
+                     <Button color="inherit" variant="outlined">
+                        Anunciar e Vender
+                     </Button>
+                  </Link>
+                  <IconButton color="secondary" onClick={(e) => { setAnchorUserMenu(e.currentTarget)}}>
+                     {
+                        true === true
+                        ? <Avatar src="" />
+                        : <AccountCircle />
+                     }
+
+                     <Typography variant="subtitle2" color="secondary" className={classes.userName}>
+                        Rafael Menezes
+                     </Typography>
+                  </IconButton>
+
+                  <Menu 
+                     anchorEl={anchorUserMenu}
+                     open={openUserMenu}
+                     onClose={() => setAnchorUserMenu(null)}
+                     anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                     }}
+                  >
+                     <Link href="/user/dashboard" passHref>
+                        <MenuItem>Meus anúncios</MenuItem>
+                     </Link>
+                     <Link href="/user/publish" passHref>
+                        <MenuItem>Pùblicar novo anúncio</MenuItem>
+                     </Link>
+                     <Divider className={classes.devider} />
+                     <Link href="/user/dashboard" passHref>
+                        <MenuItem>Sair</MenuItem>
+                     </Link>
+                  </Menu>
+
+               </Toolbar>
+            </Container>
+         </AppBar>
+      </>
+   )
 }
