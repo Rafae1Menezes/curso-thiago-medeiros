3709e7dfbac6f011dd9c45ddbe4729bcc969e311
Rafael Menezes <menezes.inbox@gmail.com>
2021-11-22 16:02:01 -0300

    cria arrossel

--- 28-projeto-final/anunx/pages/product.js
+++ 28-projeto-final/anunx/pages/product.js
@@ -1,21 +1,38 @@
-import { Avatar, Box, Card, CardHeader, CardMedia, Chip, Container, Grid, Typography } from '@material-ui/core'
+import {
+   Avatar,
+   Box,
+   Card,
+   CardHeader,
+   CardMedia,
+   Chip,
+   Container,
+   Grid,
+   Typography,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
+import Carousel from 'react-material-ui-carousel'
+
 import TemplateDefault from '../src/templates/Default'
 
-const useStyle = makeStyles( theme => ({
+const useStyle = makeStyles(theme => ({
    box: {
       backgroundColor: theme.palette.background.white,
       padding: theme.spacing(3),
-      marginBottom: theme.spacing(3)
+      marginBottom: theme.spacing(3),
    },
    productName: {
-      margin: "15px 0"
+      margin: '15px 0',
    },
    price: {
       fontWeight: 'bold',
       marginBottom: '15px',
-   }
-
+   },
+   card: {
+      height: '100%',
+   },
+   cardMedia: {
+      paddingTop: '56%',
+   },
 }))
 
 const Product = () => {
@@ -27,39 +44,85 @@ const Product = () => {
             <Grid container spacing={3}>
                <Grid item xs={8}>
                   <Box className={classes.box}>
-                     xxxx
+                     <Carousel
+                        autoPlay={false}
+                        animation= "slide"
+                        navButtonsAlwaysVisible={true}
+                        navButtonsProps={{
+                           // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+                           style: {
+                              color: 'white'
+                           },
+                        }}
+                     >
+                        <Card className={classes.card}>
+                           <CardMedia
+                              className={classes.cardMedia}
+                              image="https://source.unsplash.com/random?a=1"
+                              title="Titulo da imagem"
+                           />
+                        </Card>
+                        <Card className={classes.card}>
+                           <CardMedia
+                              className={classes.cardMedia}
+                              image="https://source.unsplash.com/random?a=2"
+                              title="Titulo da imagem"
+                           />
+                        </Card>
+                     </Carousel>
                   </Box>
                   <Box className={classes.box} textAlign="left">
-                     <Typography component="span" variant="caption">Publicado 16 junho de 2021</Typography>
-                     <Typography component="h4" variant="h4" className={classes.productName}>Jaguar XE 2.0 D R-Sport Aut.</Typography>
-                     <Typography component="h4" variant="h4" className={classes.price}>R$ 50.000,00</Typography>
+                     <Typography component="span" variant="caption">
+                        Publicado 16 junho de 2021
+                     </Typography>
+                     <Typography
+                        component="h4"
+                        variant="h4"
+                        className={classes.productName}
+                     >
+                        Jaguar XE 2.0 D R-Sport Aut.
+                     </Typography>
+                     <Typography
+                        component="h4"
+                        variant="h4"
+                        className={classes.price}
+                     >
+                        R$ 50.000,00
+                     </Typography>
                      <Chip label="Categoria" />
                   </Box>
 
                   <Box className={classes.box} textAlign="left">
-                     <Typography component="h6" variant="h6">Descrição</Typography>
+                     <Typography component="h6" variant="h6">
+                        Descrição
+                     </Typography>
                      <Typography component="p" variant="body2">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                     </Typography>                     
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the industry
+                        standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it
+                        to make a type specimen book. It has survived not only
+                        five centuries, but also the leap into electronic
+                     </Typography>
                   </Box>
                </Grid>
                <Grid item xs={4}>
                   <Card elevation={0} className={classes.box}>
                      <CardHeader
-                        avatar={
-                           <Avatar>T</Avatar>
-                        }
+                        avatar={<Avatar>T</Avatar>}
                         title="Thiago Medeiros"
                         subheader="menezes.inbox@gmail.com"
                      />
-                     <CardMedia 
+                     <CardMedia
                         image="https://source.unsplash.com/random"
                         title="Thiago Medeiros"
                      />
                   </Card>
 
                   <Box className={classes.box}>
-                     <Typography component="h6" variant="h6">Localização</Typography> 
+                     <Typography component="h6" variant="h6">
+                        Localização
+                     </Typography>
                   </Box>
                </Grid>
             </Grid>
