dfdc3aa1a85b4b52eb8b8fbd85c8004215cf6969
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-02 16:14:31 -0300

    atualiza barra de categorias

--- 28-projeto-final/anunx2/src/components/CategoryBar.js
+++ 28-projeto-final/anunx2/src/components/CategoryBar.js
@@ -21,7 +21,7 @@ const Item = styled(ButtonMui)({
 })
 
 const CategoryBar = () => {
-   const [category, setCategory] = useState('')
+   const [category, setCategory] = useState('Todas')
 
    const handleChange = event => {
       setCategory(event.target.value)
@@ -29,12 +29,18 @@ const CategoryBar = () => {
 
    return (
       <Grid container sx={{ flexGrow: 1 }}>
-         <Grid item md="9" sm="8" xs="12"><Typography component="div" variant="h4">Produtos de Bebê</Typography></Grid>
-         <Grid item md="0" sm="0" xs="12"><br /></Grid>
-         <Grid item md="3" sm="4" xs="12" align="right">
+         <Grid item md={9} sm={8} xs={12}>
+            <Typography component="div" variant="h4">
+               {category}
+            </Typography>
+         </Grid>
+         <Grid item md={0.1} sm={0.1} xs={12}>
+            <br />
+         </Grid>
+         <Grid item md={2.9} sm={3.9} xs={12} align="right">
             <FormControl fullWidth>
                <InputLabel id="demo-simple-select-label">
-               Filtrar por Categoria
+                  Filtrar por Categoria
                </InputLabel>
                <Select
                   labelId="demo-simple-select-label"
@@ -42,12 +48,34 @@ const CategoryBar = () => {
                   value={category}
                   label="Filtrar por Categoria"
                   onChange={handleChange}
-                  sx={{height: "45px"}}
+                  sx={{ height: '45px' }}
                >
-                  <MenuItem value="Todas" selected>TOdas</MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  <MenuItem value="Todas">
+                     Todas
+                  </MenuItem>
+                  <MenuItem value="Bebê e Criança">Bebê e Criança</MenuItem>
+                  <MenuItem value="Agricultura">Agricultura</MenuItem>
+                  <MenuItem value="Moda">Moda</MenuItem>
+                  <MenuItem value="Carros, Motos e Barcos">
+                     Carros, Motos e Barcos
+                  </MenuItem>
+                  <MenuItem value="Serviços">Serviços</MenuItem>
+                  <MenuItem value="Lazer">Lazer</MenuItem>
+                  <MenuItem value="Animais">Animais</MenuItem>
+                  <MenuItem value="Moveis, Casa e Jardim">
+                     Moveis, Casa e Jardim
+                  </MenuItem>
+                  <MenuItem value="Imóveis">Imóveis</MenuItem>
+                  <MenuItem value="Equipamentos e Ferramentas">
+                     Equipamentos e Ferramentas
+                  </MenuItem>
+                  <MenuItem value="Celulares e Tablets">
+                     Celulares e Tablets
+                  </MenuItem>
+                  <MenuItem value="Esporte">Esporte</MenuItem>
+                  <MenuItem value="Tecnologia">Tecnologia</MenuItem>
+                  <MenuItem value="Emprego">Emprego</MenuItem>
+                  <MenuItem value="Outros">Outros</MenuItem>
                </Select>
             </FormControl>
          </Grid>
